a929164bc56283bf6d99d4aff3f13652a95840e6
sagarv-vitara <89578890+sagarv-vitara@users.noreply.github.com>
2023-01-23 22:40:30 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { AddresPipe } from './shared/addres.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AddresPipe
   ],
   imports: [
     BrowserModule,
